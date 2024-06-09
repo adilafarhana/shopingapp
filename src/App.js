b65c70c12d7e5121c6eb553b2a31a1975ae0c456
adilafarhana <adilafarhanavv@gmail.com>
2024-06-10 00:32:33 +0530

    ok

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
+import AddProduct from './component/AddProduct';
+import SearchProduct from './component/SearchProduct';
+import DeleteProduct from './component/DeleteProduct';
+import ViewProduct from './component/ViewProduct';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <BrowserRouter>
+    <Routes>
+      <Route path='/'element={<AddProduct/>}/>
+      <Route path='/search'element={<SearchProduct/>}/>
+      <Route path='/delete'element={<DeleteProduct/>}/>
+      <Route path='/viewAll'element={<ViewProduct/>}/>
+    </Routes>
+    </BrowserRouter>
+  )
 }
 
 export default App;
---
+++ src/component/AddProduct.jsx
@@ -0,0 +1,58 @@
+import React from 'react'
+import NavProduct from './NavProduct'
+
+const AddProduct = () => {
+  return (
+    <div>
+        <NavProduct/>
+        <div className="container">
+            <div className="row">
+                <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                    <div className="row g-3">
+                        <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                            <label htmlFor="" className="form-label">id</label>
+                            <input type="text" className="form-control" />
+                        </div>
+                        <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                            <label htmlFor="" className="form-label">title</label>
+                            <input type="text" className="form-control" />
+                        </div>
+                        <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                            <label htmlFor="" className="form-label">price</label>
+                            <input type="text" className="form-control" />
+                        </div>
+                        <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                            <label htmlFor="" className="form-label">description</label>
+                            <textarea name="" id="" className="form-control"></textarea>
+                        </div>
+                        <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                            <label htmlFor="" className="form-label">category</label>
+                            <select name="" id="" className="form-control">
+                                <option value="dress">dress</option>
+                                <option value="electronics">electronics</option>
+                                <option value="toys">toys</option>
+                                <option value="cosmetics">cosmetics</option>
+                            </select>
+                        </div>
+                        
+                        <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                            <label htmlFor="" className="form-label">image</label>
+                            <input type="file" name="" id="" className="-control" />
+                        </div>
+                        <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                            <label htmlFor="" className="form-label">rating</label>
+                            <input type="text" className="form-control" />
+                        </div>
+                        <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                            <button className="btn btn-success">Submit</button>
+                        </div>
+                       
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default AddProduct
